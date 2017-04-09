b1f33cb4864927f5be2afce995d8283fd50e21a8
aldiandika <aldi.andika@gmail.com>
2017-04-10 00:45:54 +0700

    Awal

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -11,6 +11,21 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String[] angka = new String[10];
+        angka[0] = "one";
+        angka[1] = "Two";
+        angka[2] = "Three";
+        angka[3] = "Four";
+        angka[4] = "Five";
+        angka[5] = "Six";
+        angka[6] = "Seven";
+        angka[7] = "Eight";
+        angka[8] = "Nine";
+        angka[9] = "Ten";
+
+
+
     }
 
 }
